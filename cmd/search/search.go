ed3c1c2be1428bbc14ac3c4a76d639e76526cb87
robbykansas <lomiver@gmail.com>
2024-08-13 16:20:54 +0700

    feat: use goroutine for search

--- cmd/search/search.go
+++ cmd/search/search.go
@@ -4,27 +4,54 @@ import (
 	"fmt"
 	"robbykansas/another-novel-scraper/cmd/flags"
 	"robbykansas/another-novel-scraper/cmd/sources"
+
+	"sync"
 )
 
+var AllWebInfo = []flags.WebInfo{
+	sources.NovelhallInfo,
+	sources.FirstKissNovelInfo,
+}
+
+var AllSources = map[string]func(string, flags.WebInfo, *sync.WaitGroup, chan<- []flags.NovelData, chan<- error){
+	"Novelhall":    sources.NovelhallSearch,
+	"1stKissNovel": sources.FirstKissNovelSearch,
+}
+
 func SearchTitle(title string) (map[string][]flags.NovelData, error) {
+	var wg sync.WaitGroup
+	var channelRes = make(chan []flags.NovelData, 2)
+	var channelErr = make(chan error)
 	groupedTitle := make(map[string][]flags.NovelData)
-	AllSources := map[string]func(string, *flags.NovelInfo) ([]flags.NovelData, error){
-		"Novelhall":    sources.NovelhallSearch,
-		"1stKissNovel": sources.FirstKissNovelSearch,
+
+	for _, search := range AllWebInfo {
+		wg.Add(1)
+		go AllSources[search.WebName.String()](title, search, &wg, channelRes, channelErr)
 	}
 
-	for _, search := range flags.WebInfo {
-		result, err := AllSources[search.WebName.String()](title, &search)
+	go func() {
+		wg.Wait()
+		close(channelErr)
+		close(channelRes)
+	}()
+
+	for err := range channelErr {
 		if err != nil {
-			fmt.Println(err, "<<<<<<< error this")
-		}
-
-		for _, g := range result {
-			groupedTitle[g.Title] = append(groupedTitle[g.Title], g)
+			fmt.Println(err, "<<<<<<<<<< error")
 		}
 	}
 
-	// fmt.Printf("%+v\n", groupedTitle)
+	for res := range channelRes {
+		if len(res) > 0 {
+			for _, g := range res {
+				groupedTitle[g.Title] = append(groupedTitle[g.Title], g)
+			}
+		}
+	}
 
-	return groupedTitle, nil
+	if len(groupedTitle) > 0 {
+		return groupedTitle, nil
+	} else {
+		return nil, fmt.Errorf("Error")
+	}
 }
