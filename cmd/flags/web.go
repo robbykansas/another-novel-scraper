1dfc8a223e66fbeb5d9ade8c64b1ae30e6272acb
robbykansas <lomiver@gmail.com>
2024-08-13 16:20:14 +0700

    feat: change set range

--- cmd/flags/web.go
+++ cmd/flags/web.go
@@ -6,28 +6,7 @@ import (
 
 type Web string
 
-type NovelData struct {
-	WebName          string
-	Title            string
-	Url              string
-	AvailableChapter string
-}
-
-type NovelInfo struct {
-	WebName   Web
-	SearchUrl string
-}
-
-var WebInfo = []NovelInfo{
-	{
-		WebName:   "Novelhall",
-		SearchUrl: "https://www.novelhall.com/index.php?s=so&module=book&keyword=%s",
-	},
-	{
-		WebName:   "1stKissNovel",
-		SearchUrl: "https://1stkissnovel.org/?s=%s&post_type=wp-manga",
-	},
-}
+var AllowedWeb []string
 
 // var AvailableWeb = fmt.Sprintf("available Web for scraping: %s", strings.Join(ListWeb, ", "))
 var errorScraping = "Error Scraping"
@@ -41,8 +20,8 @@ func (n *Web) Type() string {
 }
 
 func (n *Web) Set(value string) error {
-	for _, web := range WebInfo {
-		if string(web.WebName) == value {
+	for _, web := range AllowedWeb {
+		if string(web) == value {
 			*n = Web(value)
 			return nil
 		}
