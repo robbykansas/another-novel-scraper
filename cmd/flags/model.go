2befb5452bf9ec905f04eab5d019746cc7717a23
robbykansas <lomiver@gmail.com>
2024-08-13 16:19:47 +0700

    feat: move struct

---
+++ cmd/flags/model.go
@@ -0,0 +1,13 @@
+package flags
+
+type NovelData struct {
+	WebName          string
+	Title            string
+	Url              string
+	AvailableChapter string
+}
+
+type WebInfo struct {
+	WebName   Web
+	SearchUrl string
+}
