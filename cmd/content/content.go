0dc43802ce33e93a7fa74b98dab92a0f3892115a
robbykansas <lomiver@gmail.com>
2024-08-14 19:11:30 +0700

    feat: add get content

---
+++ cmd/content/content.go
@@ -0,0 +1,21 @@
+package content
+
+import (
+	"robbykansas/another-novel-scraper/cmd/epub"
+	"robbykansas/another-novel-scraper/cmd/sources"
+	"strings"
+)
+
+var AllContent = map[string]func(string, string) *sources.NovelInfo{
+	"Novelhall": sources.NovelhallContent,
+}
+
+func GetContent(content string, folder string, title string) {
+	dataContent := strings.Split(content, ",")
+	WebName := dataContent[0]
+	Url := dataContent[1]
+
+	epubData := AllContent[WebName](Url, title)
+
+	epub.SetEpub(folder, epubData)
+}
