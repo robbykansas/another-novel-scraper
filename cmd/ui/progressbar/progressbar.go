a6c277c8f57de3da19fef0da00162fbe5b2e9dc4
robbykansas <lomiver@gmail.com>
2024-09-12 11:42:19 +0700

    feat: add progressbar ui

---
+++ cmd/ui/progressbar/progressbar.go
@@ -0,0 +1,82 @@
+package progressbar
+
+import (
+	"strings"
+
+	"github.com/charmbracelet/bubbles/progress"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+	"github.com/shopspring/decimal"
+)
+
+const (
+	padding  = 2
+	maxWidth = 80
+)
+
+var helpStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("#626262")).Render
+
+type ProgressMsg struct{}
+type ProgressState bool
+
+type model struct {
+	progress   progress.Model
+	increments float64
+	finished   ProgressState
+}
+
+func InitialModel(total int) model {
+	progress := progress.New(progress.WithDefaultGradient())
+	incrementProgress := decimal.NewFromInt(1).Div(decimal.NewFromInt(int64(total)))
+	increment, _ := incrementProgress.Float64()
+	return model{
+		progress:   progress,
+		increments: increment,
+		finished:   false,
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		return m, tea.Quit
+
+	case tea.WindowSizeMsg:
+		m.progress.Width = msg.Width - padding*2 - 4
+		if m.progress.Width > maxWidth {
+			m.progress.Width = maxWidth
+		}
+		return m, nil
+
+	case ProgressMsg:
+		if m.progress.Percent() >= 1.0 {
+			return m, tea.Quit
+		}
+
+		// Note that you can also use progress.Model.SetPercent to set the
+		// percentage value explicitly, too.
+		cmd := m.progress.IncrPercent(m.increments)
+
+		return m, cmd
+
+	// FrameMsg is sent when the progress bar wants to animate itself
+	case progress.FrameMsg:
+		progressModel, cmd := m.progress.Update(msg)
+		m.progress = progressModel.(progress.Model)
+		return m, cmd
+
+	default:
+		return m, nil
+	}
+}
+
+func (m model) View() string {
+	pad := strings.Repeat(" ", padding)
+	return "\n" +
+		pad + m.progress.View() + "\n\n" +
+		pad + helpStyle("Press any key to quit")
+}
