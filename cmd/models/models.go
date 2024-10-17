aefe0b19d4a11d461fd59028edbec9553864393f
robbykansas <lomiver@gmail.com>
2024-10-17 13:47:19 +0700

    feat: rename model to models

---
+++ cmd/models/models.go
@@ -0,0 +1,36 @@
+package models
+
+import "sync"
+
+type ListChapter struct {
+	Order   int
+	Title   string
+	Url     string
+	Content string
+}
+
+type NovelInfo struct {
+	Title    string
+	Image    string
+	Author   string
+	Synopsis string
+	Data     []ListChapter
+}
+
+type Web string
+
+type WebInfo struct {
+	WebName   Web
+	SearchUrl string
+}
+
+var MapSearch = make(map[string]func(string, *sync.WaitGroup, chan<- []NovelData, chan<- error))
+var MapToc = make(map[string]func(string, string) *NovelInfo)
+var MapContent = make(map[string]func(ListChapter, *sync.WaitGroup, chan<- ListChapter))
+
+type NovelData struct {
+	WebName          string
+	Title            string
+	Url              string
+	AvailableChapter string
+}
