8bc221102d85129772b0890a9664f46ce6a2a7fe
robbykansas <lomiver@gmail.com>
2025-04-18 16:51:09 +0700

    feat: add worker pool instead of normal func

--- cmd/models/models.go
+++ cmd/models/models.go
@@ -1,6 +1,10 @@
 package models
 
-import "sync"
+import (
+	"strings"
+	"sync"
+	"time"
+)
 
 type ListChapter struct {
 	Order   int
@@ -25,6 +29,75 @@ type WebInfo struct {
 	SearchUrl string
 }
 
+type NovelData struct {
+	WebName          string
+	Title            string
+	Url              string
+	AvailableChapter string
+}
+
+type WorkerPoolContent struct {
+	List        []ListChapter
+	Concurrency int
+	Wg          sync.WaitGroup
+	ch          chan *ListChapter // give task
+	Res         chan *ListChapter // get result
+	Pool        *sync.Pool
+}
+
+func ListContent(content string, title string) *NovelInfo {
+	split := strings.Split(content, ",")
+	WebName := split[0]
+	Url := split[1]
+
+	list := MapToc[WebName](Url, title)
+	return list
+}
+
+func (wp *WorkerPoolContent) worker(web string) {
+	for task := range wp.ch {
+		go MapContent[web](task, wp)
+	}
+}
+
+func (wp *WorkerPoolContent) Run(content string, title string) {
+	// initialize the tasks channel and pool
+	wp.ch = make(chan *ListChapter, 10)
+	wp.Res = make(chan *ListChapter, 10)
+	wp.Pool = &sync.Pool{
+		New: func() interface{} {
+			return &ListChapter{}
+		},
+	}
+
+	split := strings.Split(content, ",")
+	WebName := split[0]
+
+	list := ListContent(content, title)
+
+	// start workers
+	for i := 0; i < wp.Concurrency; i++ {
+		go wp.worker(WebName)
+	}
+
+	// send tasks to the channel
+	for i := range list.Data {
+		wp.Wg.Add(1)
+		wp.ch <- &list.Data[i]
+
+		switch WebName {
+		case "Novelbin", "NovelAll": //has anti scraping which makes it can't scrape too fast or got too many request
+			time.Sleep(1000 * time.Millisecond)
+		default:
+			time.Sleep(10 * time.Millisecond)
+		}
+	}
+
+	wp.Wg.Wait()
+	close(wp.ch)
+	close(wp.Res)
+}
+
 func (c *ListChapter) Reset() {
 	c.Order = 0
 	c.Title = ""
@@ -36,11 +109,4 @@ var DefaultPath string = "./ans-config"
 
 var MapSearch = make(map[string]func(string, *sync.WaitGroup, chan<- []NovelData, chan<- error))
 var MapToc = make(map[string]func(string, string) *NovelInfo)
-var MapContent = make(map[string]func(*ListChapter, *sync.WaitGroup, chan<- *ListChapter, *sync.Pool))
-
-type NovelData struct {
-	WebName          string
-	Title            string
-	Url              string
-	AvailableChapter string
-}
+var MapContent = make(map[string]func(*ListChapter, *WorkerPoolContent))
